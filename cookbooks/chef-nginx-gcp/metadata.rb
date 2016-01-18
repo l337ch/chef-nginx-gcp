c700c0d27fd1e2ac72758335c24aa825a61cdee7
Lee Chang <leetchang@gmail.com>
2016-01-18 14:35:48 -0800

    initial commit with chef

---
+++ cookbooks/chef-nginx-gcp/metadata.rb
@@ -0,0 +1,7 @@
+name 'chef-nginx-gcp'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures chef-nginx-gcp'
+long_description 'Installs/Configures chef-nginx-gcp'
+version '0.1.0'
---
+++ cookbooks/chef-nginx-gcp/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: chef-nginx-gcp
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
